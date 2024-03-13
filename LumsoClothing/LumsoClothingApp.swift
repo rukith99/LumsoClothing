7003b12709ce79c0ab553761923c966130e0426e
rukith99 <rukith.ranasinghe@gmail.com>
2024-03-13 22:39:30 +0530

    Initial Commit

---
+++ LumsoClothing/LumsoClothingApp.swift
@@ -0,0 +1,17 @@
+//
+//  LumsoClothingApp.swift
+//  LumsoClothing
+//
+//  Created by Rukith Ranasinghe on 2024-03-13.
+//
+
+import SwiftUI
+
+@main
+struct LumsoClothingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
