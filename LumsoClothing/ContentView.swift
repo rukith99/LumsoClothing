7683dc2c778e1907b92cae64d0e9f3ca7a1db2a0
rukith99 <rukith.ranasinghe@gmail.com>
2024-03-26 18:42:32 +0530

    Content View Updated.

--- LumsoClothing/ContentView.swift
+++ LumsoClothing/ContentView.swift
@@ -8,8 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+@State private var selectedTab = 0
+@StateObject private var cartManager = CartManager() // Create a single instance of CartManager
+    
     var body: some View {
-        WelcomeView()
+        TabView(selection: $selectedTab) {
+            ExploreView(cartManager: cartManager) // Pass the shared cartManager instance
+                .tabItem {
+                    Label("Explore", systemImage: "magnifyingglass")
+                }
+                .tag(0)
+            
+            CartView(cartManager: cartManager) // Pass the same cartManager instance
+                .tabItem {
+                    Label("Cart", systemImage: "cart")
+                }
+                .tag(1)
+        }
     }
 }
 
